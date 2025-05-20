51467cef175a7db4f2b846b5c02769862af0586b
asimmaqsood57 <asimmaqsood57@gmail.com>
2025-05-20 21:37:02 +0500

    hostel reserve page added

---
+++ src/app/(unprotected)/property-details/[id]/property-reserve/page.tsx
@@ -0,0 +1,16 @@
+import FooterLandingPage from "@/components/LandingPage/FooterLandingPage";
+import Header from "@/components/LandingPage/Header";
+import ReservationSection from "@/components/LandingPage/ReservationSection";
+import React from "react";
+
+const PropertyReserve = () => {
+  return (
+    <div>
+      <Header />
+      <ReservationSection />
+      <FooterLandingPage />
+    </div>
+  );
+};
+
+export default PropertyReserve;
