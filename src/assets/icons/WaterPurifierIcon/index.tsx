89079b882d856e5496dad840b85ac9dc7c674588
Asim Maqsood <asimmaqsood57@gmail.com>
2025-07-21 21:38:06 +0500

    ui issue fixed

--- src/assets/icons/WaterPurifierIcon/index.tsx
+++ src/assets/icons/WaterPurifierIcon/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const WaterPurifierIcon = () => {
   return (
     <div>
-     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#003B95" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-glass-water-icon lucide-glass-water"><path d="M5.116 4.104A1 1 0 0 1 6.11 3h11.78a1 1 0 0 1 .994 1.105L17.19 20.21A2 2 0 0 1 15.2 22H8.8a2 2 0 0 1-2-1.79z"/><path d="M6 12a5 5 0 0 1 6 0 5 5 0 0 0 6 0"/></svg>
+     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#003B95" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-glass-water-icon lucide-glass-water"><path d="M5.116 4.104A1 1 0 0 1 6.11 3h11.78a1 1 0 0 1 .994 1.105L17.19 20.21A2 2 0 0 1 15.2 22H8.8a2 2 0 0 1-2-1.79z"/><path d="M6 12a5 5 0 0 1 6 0 5 5 0 0 0 6 0"/></svg>
     </div>
   );
 };
